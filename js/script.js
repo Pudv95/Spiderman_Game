df8b45ba11d764f8e3261e9b04b03e51b714289c
Pudv95 <vivekupadhayay18@gmail.com>
2023-10-07 11:23:43 +0530

    add spidy movement

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
+import Spidy from "./Models/spidy.js";
+import { health } from "./config.js";
+
 const canvas = document.getElementsByTagName("canvas")[0]
 const context = canvas.getContext("2d");
 
 canvas.width = 1100;
 canvas.height = 700;
 
+const spidy_standing = new Image();
+spidy_standing.src = "../../assets/spidy/standing.png"
+
 
 function draw(context){
     
 }
 
+const spidy = new Spidy(context);
+
 function game(){
 
-
-    draw(context);
+    spidy.update();
     requestAnimationFrame(game);
 }
 
 game();
-
